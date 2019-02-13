77c71c67b4d186caafb229bada3db24f5a6200a4
juliangriggs <jgriggs@lightstep.com>
2019-02-13 13:42:13 -0800

    Refactor the different parts of the api into their own files

--- lightstep-sdk/client.go
+++ lightstep-sdk/client.go
@@ -9,6 +9,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 )
 
 type Headers map[string]string
@@ -28,6 +29,60 @@ func (a APIClientError) Error() string {
 }
 func (a APIClientError) GetHTTPResponse() *http.Response {
 	return a.Response
+}
+
+type Body struct {
+	Data   interface{}            `json:"data,omitempty"`
+	Errors []string               `json:"errors,omitempty"`
+	Links  map[string]interface{} `json:"links,omitempty"`
+}
+
+type PublicAPIClient struct {
+	hostname    string
+	client      *http.Client
+	contentType string
+}
+
+// NewPublicAPIClient gets a client for the public API
+func NewPublicAPIClient(ctx context.Context, hostname string) *PublicAPIClient {
+	cookieJar, err := cookiejar.New(nil)
+	if err != nil {
+		log.Fatal(ctx, err)
+	}
+	return &PublicAPIClient{
+		hostname: hostname,
+		client: &http.Client{
+			Jar: cookieJar,
+		},
+		contentType: "application/vnd.api+json",
+	}
+}
+
+// CallAPI calls the given API and unmarshals the result to into result.
+func (c *PublicAPIClient) CallAPI(
+	httpMethod string,
+	suffix string,
+	authToken string,
+	data interface{},
+	result interface{},
+) error {
+	return callAPI(
+		context.TODO(),
+		c.client,
+		generateURLForPublicAPI(c.hostname, suffix),
+		httpMethod,
+		Headers{
+			"Authorization": fmt.Sprintf("bearer %v", authToken),
+			"Content-Type":  c.contentType,
+			"Accept":        c.contentType,
+		},
+		data,
+		result,
+	)
+}
+
+func generateURLForPublicAPI(hostname string, suffix string) string {
+	return fmt.Sprintf("%v/public/v0.1/%v", hostname, suffix)
 }
 
 func executeAPIRequest(client *http.Client, req *http.Request, result interface{}) error {
@@ -83,6 +138,7 @@ func executeAPIRequest(client *http.Client, req *http.Request, result interface{
 
 	return nil
 }
+
 func createJSONRequest(
 	ctx context.Context,
 	httpMethod string,
