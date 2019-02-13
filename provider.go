aa2778a5b84c1f21cc9c99fff351d7c43f655718
Ishmeet Grewal <ishmeetsinghghrewal@gmail.com>
2019-02-13 12:03:42 -0800

    Go Modules init

--- provider.go
+++ provider.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/schema"
 )
 
 func Provider() *schema.Provider {
-  return &schema.Provider{
-    ResourcesMap: map[string]*schema.Resource{
-      "lightstep_project": resourceProject()
-    },
-  }
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{
+			"lightstep_project": resourceProject(),
+		},
+	}
 }
