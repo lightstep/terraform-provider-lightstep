cd51dc2aac67ffbf63b1ef61d259cbd7d82a9210
Cody Boppert <cody@codyboppert.nyc>
2019-02-13 17:54:56 -0500

    Still making up stuff as I go

--- resource_lightstep_dashboard.go
+++ resource_lightstep_dashboard.go
@@ -8,9 +8,24 @@ import (
 func resourceLightstepDashboard() *shema.Resource {
     return &schema.Resource {
         Create: resourceLightstepDashboardCreate,
+        Get:    resourceLightstepDashboardGet,
+        Delete: resourceLightstepDashboardDelete,
+    },
+
+    Schema: map[string]*schema.Schema {
+
     }
 }
 
 func resourceLightstepDashboardCreate(d *shema.ResourceData, meta interface{}) error {
+    client := meta.(*lightstep.Client)
 
+}
+
+func resourceLightstepDashboardGet() {
+    client := meta.(*lightstep.Client)
+}
+
+func resourceLightstepDashboardDelete() {
+    client := meta.(*lightstep.Client)
 }
