de1d0edeb9f12c4356e75753b1111959db4baaad
Cody Boppert <cody@codyboppert.nyc>
2019-02-13 20:43:16 -0500

    Update schema

--- provider.go
+++ provider.go
@@ -6,12 +6,26 @@ import (
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
+                Schema: map[string]*schema.Schema{
+                        "api_key": {
+                                Type: schema.TypeString,
+                                Required: true,
+                                DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_API_KEY", nil),
+                        },
+                        "base_url": {
+                                Type: schema.TypeString,
+                                Required: false,
+                                DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_BASE_URL", "https://api-staging.lightstep.com/public/v0.1/"),
+                        },
+                        "org_name": {
+                                Type: schema.TypeString,
+                                Required: true,
+                                DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_ORG_NAME", nil),
+                        }
+                },
 		ResourcesMap: map[string]*schema.Resource{
 			"lightstep_stream": resourceStream(),
+                        "lightstep_dashboard": resourceLightstepDashboard(),
 		},
-
-                ResourcesMap: map[string]*schema.Resource{
-                    "lightstep_dashboard": resourceLightstepDashboard(),
-                },
 	}
 }
