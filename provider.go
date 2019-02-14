359ed09cf858609ffd910891cd756ad3a13f677c
Ishmeet Grewal <ishmeetsinghghrewal@gmail.com>
2019-02-14 11:39:31 -0800

    Minimum CRUD for Streams

--- provider.go
+++ provider.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "context"
-  "log"
+	"context"
 	"github.com/hashicorp/terraform/helper/schema"
-  "github.com/lightstep/terraform-provider-lightstep/lightstep"
+	"github.com/lightstep/terraform-provider-lightstep/lightstep"
+	"log"
 )
 
 func Provider() *schema.Provider {
 	return &schema.Provider{
-    Schema: map[string]*schema.Schema{
-      "api_key": {
-        Type:        schema.TypeString,
-        Required:    true,
-        DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_API_KEY", nil),
-      },
-      "organization": {
-        Type:        schema.TypeString,
-        Required:    true,
-        DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_HOST", nil),
-      },
-    },
+		Schema: map[string]*schema.Schema{
+			"api_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_API_KEY", nil),
+			},
+			"organization": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("LIGHTSTEP_HOST", nil),
+			},
+		},
 
 		ResourcesMap: map[string]*schema.Resource{
 			"lightstep_project": resourceProject(),
-      // "lightstep_dashboard": resourceDashboard(),
-      "lightstep_stream": resourceStream(),
+			// "lightstep_dashboard": resourceDashboard(),
+			"lightstep_stream": resourceStream(),
 		},
 
-    ConfigureFunc: configureProvider,
+		ConfigureFunc: configureProvider,
 	}
 }
 
 func configureProvider(d *schema.ResourceData) (interface{}, error) {
-  // Initializing client here with credentials
-  client := lightstep.NewClient(
-    context.Background(),
-    d.Get("api_key").(string),
-    d.Get("organization").(string),
-  )
-  log.Println("[INFO] LightStep client successfully initialized.")
+	// Initializing client here with credentials
+	client := lightstep.NewClient(
+		context.Background(),
+		d.Get("api_key").(string),
+		d.Get("organization").(string),
+	)
+	log.Println("[INFO] LightStep client successfully initialized.")
 
-  return client, nil
+	return client, nil
 }
--- resource_stream.go
+++ resource_stream.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "log"
 	"github.com/hashicorp/terraform/helper/schema"
-  "github.com/lightstep/terraform-provider-lightstep/lightstep"
+	"github.com/lightstep/terraform-provider-lightstep/lightstep"
+	"log"
 )
 
 func resourceStream() *schema.Resource {
@@ -12,68 +12,80 @@ func resourceStream() *schema.Resource {
 		Read:   resourceStreamRead,
 		Update: resourceStreamUpdate,
 		Delete: resourceStreamDelete,
-    Exists: resourceStreamExists,
+		Exists: resourceStreamExists,
 
 		Schema: map[string]*schema.Schema{
 			"project": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
-      "stream_id": &schema.Schema{
-        Type:     schema.TypeString,
-        Optional: true,
-      },
-      "name": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-      },
-      "query": &schema.Schema{
-        Type:     schema.TypeString,
-        Required: true,
-      },
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"query": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
 		},
 	}
 }
 
 func resourceStreamExists(d *schema.ResourceData, m interface{}) (b bool, e error) {
-  return false, nil
+	client := m.(*lightstep.Client)
+
+	if _, err := client.GetSearch(
+		d.Get("project").(string),
+		d.Id(),
+	); err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
 
 func resourceStreamCreate(d *schema.ResourceData, m interface{}) error {
-  client := m.(*lightstep.Client)
-  resp, err := client.CreateSearch(
-    d.Get("project").(string),
-    d.Get("name").(string),
-    d.Get("query").(string),
-    nil,
-  )
-  if err != nil {
-    log.Println(err)
-    return err
-  }
-  log.Println(resp.Data.ID)
-  d.SetId(string(resp.Data.ID))
-  d.Set("stream_id", string(resp.Data.ID))
+	client := m.(*lightstep.Client)
+	resp, err := client.CreateSearch(
+		d.Get("project").(string),
+		d.Get("name").(string),
+		d.Get("query").(string),
+		nil,
+	)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	d.SetId(string(resp.Data.ID))
 	return resourceStreamRead(d, m)
 }
 
 func resourceStreamRead(d *schema.ResourceData, m interface{}) error {
 	client := m.(*lightstep.Client)
-  _, err := client.GetSearch(
-    d.Get("project").(string),
-    d.Id(),
-  )
-  if err != nil {
-    return err
-  }
-  return nil
+	_, err := client.GetSearch(
+		d.Get("project").(string),
+		d.Id(),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func resourceStreamUpdate(d *schema.ResourceData, m interface{}) error {
-	return resourceStreamRead(d, m)
+	return resourceStreamCreate(d, m)
 }
 
 func resourceStreamDelete(d *schema.ResourceData, m interface{}) error {
-  d.SetId("")
+
+	client := m.(*lightstep.Client)
+	err := client.DeleteSearch(
+		d.Get("project").(string),
+		d.Id(),
+	)
+	if err != nil {
+		return err
+	}
+	d.SetId("")
 	return nil
 }
