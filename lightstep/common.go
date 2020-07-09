a8261b936e6d4c3a1f9b6b3d061c2255a5baeb74
paigerduty <pbernier@lightstep.com>
2020-07-09 16:09:09 -0700

    wip

--- lightstep/common.go
+++ lightstep/common.go
@@ -2,25 +2,13 @@ package lightstep
 
 import "encoding/json"
 
-type Response struct {
-	Type string `json:"type"`
-	ID   string `json:"id"`
+type Envelope struct {
+	Data  json.RawMessage `json:"data"`
+	Links Links           `json:"links,omitempty"`
 }
-
-type ResourceIDObject struct {
-	Response
-}
-
-type APIResponse struct {
-	Data string `json:"data"`
-}
-type Links map[string]string
 
 type LinksObj struct {
 	Links Links `json:"links"`
 }
 
-type Envelope struct {
-	Data  json.RawMessage `json:"data"`
-	Links Links           `json:"links,omitempty"`
-}
+type Links map[string]string
--- lightstep/conditions.go
+++ lightstep/conditions.go
@@ -1,18 +1,21 @@
 package lightstep
 
-type ConditionAPIResponse struct {
-	Data *ConditionResponse
-}
+import (
+	"encoding/json"
+	"fmt"
 
-type ConditionResponse struct {
-	Response
-	Attributes    ConditionAttributes    `json:"attributes,omitempty"`
+)
+
+type Condition struct {
+	Type          string                 `json:"type,omitempty"`
+	ID            string                 `json:"id,omitempty"`
+	Attributes    ConditionAttributes 	`json:"attributes"`
 	Relationships ConditionRelationships `json:"relationships,omitempty"`
 	Links         Links                  `json:"links"`
 }
 
 type ConditionAttributes struct {
-	Name               *string                `json:"name"`
+	Name               string                `json:"name"`
 	EvaluationWindowMs int64                  `json:"eval-window-ms"`
 	Expression         string                 `json:"expression"`
 	CustomData         map[string]interface{} `json:"custom-data,omitempty"`
@@ -20,52 +23,119 @@ type ConditionAttributes struct {
 
 type ConditionRelationships struct {
 	Project LinksObj `json:"project"`
-	Search  LinksObj `json:"search"`
+	Stream  LinksObj `json:"stream"`
 }
 
-type ListConditionsAPIResponse struct {
-	Data *ListConditionsResponse `json:"data,omitempty"`
+//
+//func(c *Client) CreateCondition(
+//	projectName string,
+//	conditionName string,
+//	relationships map[string]string,) (Condition, error){
+//	bytes, err := json.Marshal(
+//		Condition{
+//			Type: "condition",
+//			Attributes: ConditionAttributes{
+//				Name:    conditionName,
+//				EvaluationWindowMs: ,
+//				Expression:,
+//				CustomData: ,
+//			},
+//			Relationships: ConditionRelationships{
+//				Project:
+//					Stream:
+//			}
+//			},
+//		})
+//	if err != nil {
+//		log.Printf("error marshalling data: %v", err)
+//	}
+//
+//	var resp Envelope
+//	var cond Condition
+//
+//	req := Envelope{
+//		Data: bytes,
+//	}
+//
+//	err = c.CallAPI("POST", fmt.Sprintf("projects/%v/conditions", projectName), req, &resp)
+//	if err != nil {
+//		log.Printf("error getting dashboard: %v", err)
+//		return cond, err
+//	}
+//
+//	err = json.Unmarshal(resp.Data, &c)
+//	if err != nil {
+//		log.Printf("error unmarshalling: %v", err)
+//		return cond, err
+//	}
+//	return cond, err
+//}
+
+func (c *Client)GetCondition(projectName string, conditionID string) (Condition, error) {
+
+	var cond Condition
+	var resp Envelope
+
+	err := c.CallAPI("GET", fmt.Sprintf("projects/%v/conditions/%v", projectName, conditionID), nil, &resp)
+	if err != nil {
+		fmt.Printf("error getting condition %v", err)
+		return cond, err
+	}
+
+	err = json.Unmarshal(resp.Data, &cond)
+	if err != nil {
+		fmt.Printf("error unmarshalling: %v", err)
+		return cond, err
+	}
+	return cond, err
 }
 
-type ListConditionsResponse []ConditionResponse
+//type Condition struct {
+//	Type          string                 `json:"type,omitempty"`
+//	ID            string                 `json:"id,omitempty"`
+//	Attributes    ConditionAttributes 	`json:"attributes"`
+//	Relationships ConditionRelationships `json:"relationships,omitempty"`
+//	Links         Links                  `json:"links"`
+//}
+//
+//type ConditionAttributes struct {
+//	Name               string                `json:"name"`
+//	EvaluationWindowMs int64                  `json:"eval-window-ms"`
+//	Expression         string                 `json:"expression"`
+//	CustomData         map[string]interface{} `json:"custom-data,omitempty"`
+//}
+//
+//type ConditionRelationships struct {
+//	Project LinksObj `json:"project"`
+//	Stream  LinksObj `json:"stream"`
+//}
 
-type ConditionStatusAPIResponse struct {
-	Data *ConditionStatusResponse
+func(c *Client) UpdateCondition(projectName string, condition Condition) (Condition, error){
+	resp := Envelope{}
+
+	bytes, err := json.Marshal(condition)
+	if err != nil {
+		fmt.Printf("error marshalling %v", err)
+	}
+
+	err = c.CallAPI("PATCH", fmt.Sprintf("projects/%v/conditions/%v", projectName, condition.ID),Envelope{Data:bytes}, &resp)
+	if err != nil {
+		fmt.Printf("error calling api %v", err)
+	}
+	cond := Condition{}
+	err = json.Unmarshal(resp.Data, &cond)
+	if err != nil {
+		fmt.Printf("error unmarshalling %v", err)
+	}
+	return cond, err
 }
 
-type ConditionStatusResponse struct {
-	Response
-	Attributes    ConditionStatusAttributes    `json:"attributes,omitempty"`
-	Relationships ConditionStatusRelationships `json:"relationships,omitempty"`
+func(c *Client) DeleteCondition(projectName string, conditionID string) error {
+	err := c.CallAPI("DELETE", fmt.Sprintf("projects/%v/conditions/%v", projectName, conditionID), nil, nil)
+	if err != nil {
+		fmt.Printf("error getting condition %v", err)
+	}
+	return err
 }
 
-type ConditionStatusAttributes struct {
-	Expression  string `json:"expression"`
-	State       string `json:"state"`
-	Description string `json:"description"`
-}
 
-type ConditionStatusRelationships struct {
-	Condition LinksObj `json:"condition"`
-}
-
-type ConditionRequestBody struct {
-	Data *ConditionRequest `json:"data"`
-}
-
-type ConditionRequest struct {
-	Response
-	Attributes    ConditionRequestAttributes    `json:"attributes"`
-	Relationships ConditionRequestRelationships `json:"relationships"`
-}
-
-type ConditionRequestAttributes struct {
-	Name               *string                 `json:"name"`
-	Expression         *string                 `json:"expression"`
-	EvaluationWindowMs *int64                  `json:"eval-window-ms"`
-	CustomData         *map[string]interface{} `json:"custom-data"`
-}
-
-type ConditionRequestRelationships struct {
-	Search ResourceIDObject `json:"search"`
-}
