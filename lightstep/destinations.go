939d2e7d6df653660890c33e0a7a7066f4fd39e4
paigerduty <pbernier@lightstep.com>
2020-08-18 13:41:08 -0700

    remove unused vars

--- lightstep/destinations.go
+++ lightstep/destinations.go
@@ -6,14 +6,6 @@ import (
 	"net/http"
 )
 
-type DestinationType string
-
-const (
-	PAGERDUTY_DESTINATION_TYPE DestinationType = "pagerduty"
-	WEBHOOK_DESTINATION_TYPE   DestinationType = "webhook"
-	SLACK_DESTINATION_TYPE     DestinationType = "slack"
-)
-
 type Destination struct {
 	Type       string      `json:"type"`
 	Attributes interface{} `json:"attributes"`
@@ -22,7 +14,7 @@ type Destination struct {
 
 type WebhookAttributes struct {
 	Name            string                 `json:"name"`
-	DestinationType DestinationType        `json:"destination_type"`
+	DestinationType string                 `json:"destination_type"`
 	URL             string                 `json:"url"`
 	CustomHeaders   map[string]interface{} `json:"custom_headers,omitempty"`
 }
