82cb6453d7be681314a2d3c276e4e8e11fa82acf
paigerduty <pbernier@lightstep.com>
2020-08-19 18:13:28 -0700

    fixes from commenting

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 variable "project" {
   type    = string
-  default = "YOUR STAGING PROJECT HERE"
+  default = "YOUR PROJECT HERE"
 }
 
 ##############################################################
