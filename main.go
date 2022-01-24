bc09a484e7c9568046abbdb4621781bb19afdc50
Clay Smith <smithclay@gmail.com>
2022-01-24 11:14:23 -0800

    LS-19000: add ability to export metric dashboards (#64)

--- main.go
+++ main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/lightstep/terraform-provider-lightstep/exporter"
 	"github.com/lightstep/terraform-provider-lightstep/lightstep"
+	"log"
+	"os"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "exporter" {
+		if err := exporter.Run(os.Args...); err != nil {
+			log.Printf("[ERROR] %s", err.Error())
+			os.Exit(1)
+		}
+		return
+	}
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
 			return lightstep.Provider()
