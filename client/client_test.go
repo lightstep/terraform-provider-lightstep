ae7a3d1731995eb6af510e5791ccd6f9c9720c44
Greg Dallavalle <greg.dallavalle@lightstep.com>
2022-12-08 14:57:40 -0600

    feat: allow LIGHTSTEP_API_BASE_URL to override API host

--- client/client_test.go
+++ client/client_test.go
@@ -17,3 +17,10 @@ func TestNew_other(t *testing.T) {
 	c := NewClient("api-key", "org-name", "other")
 	assert.Equal(t, "https://api-other.lightstep.com/public/v0.2/org-name", c.baseURL)
 }
+
+func TestNew_env_var_provided_baseURL(t *testing.T) {
+	// Parallel not used here due to t.Setenv.
+	t.Setenv("LIGHTSTEP_API_BASE_URL", "http://localhost:8080")
+	c := NewClient("api-key", "org-name", "public")
+	assert.Equal(t, "http://localhost:8080/public/v0.2/org-name", c.baseURL)
+}
