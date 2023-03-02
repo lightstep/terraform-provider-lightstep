56d06badb78e072b252b52801fdb7200a39585ad
Carolyn Blumberg <carolyn.blumberg@lightstep.com>
2023-03-02 16:52:27 -0500

    fix typos in test

--- lightstep/resource_dashboard_test.go
+++ lightstep/resource_dashboard_test.go
@@ -134,25 +134,26 @@ resource "lightstep_dashboard" "test" {
 `
 
 	templateVariableDashboardConfig := `
-resource "lightstep_metric_dashboard" "test" {
+resource "lightstep_dashboard" "test" {
   project_name          = "terraform-provider-tests"
   dashboard_name        = "Acceptance Test Dashboard"
-  dashboard_description = "Dashboard to test if the terraform provider works"
+
   chart {
     name = "Chart Number One"
     rank = 1
     type = "timeseries"
+
     query {
       hidden              = false
       query_name          = "a"
       display             = "line"
-      tql                 = "metric m | filter (service == $service) | rate | group_by [], sum"
+      query_string        = "metric m | filter (service == $service) | rate | group_by [], sum"
     }
   }
   template_variable {
 	name = "service"
 	default_values = ["myService"]
-    suggestion_attribute_key = "service.name"
+    suggestion_attribute_key = "service_name"
   }
 }
 `
@@ -216,13 +217,12 @@ resource "lightstep_metric_dashboard" "test" {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckMetricDashboardExists(resourceName, &dashboard),
 					resource.TestCheckResourceAttr(resourceName, "dashboard_name", "Acceptance Test Dashboard"),
-					resource.TestCheckResourceAttr(resourceName, "dashboard_description", "Dashboard to test if the terraform provider works"),
-					resource.TestCheckResourceAttr(resourceName, "chart.0.query.0.tql", "metric m | filter (service == $service) | rate | group_by [], sum"),
+					resource.TestCheckResourceAttr(resourceName, "chart.0.query.0.query_string", "metric m | filter (service == $service) | rate | group_by [], sum"),
 					resource.TestCheckResourceAttr(resourceName, "chart.0.query.0.display", "line"),
 					resource.TestCheckResourceAttr(resourceName, "chart.0.query.0.hidden", "false"),
 					resource.TestCheckResourceAttr(resourceName, "template_variable.0.name", "service"),
 					resource.TestCheckResourceAttr(resourceName, "template_variable.0.default_values.0", "myService"),
-					resource.TestCheckResourceAttr(resourceName, "template_variable.0.suggestion_attribute_key", "service.name"),
+					resource.TestCheckResourceAttr(resourceName, "template_variable.0.suggestion_attribute_key", "service_name"),
 				),
 			},
 		},
