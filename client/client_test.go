efc53fafc76323ecdbf0680f205747c16ef78156
Matt Hagenbuch <matt.hagenbuch@lightstep.com>
2024-02-12 12:52:58 -0500

    rm

--- client/client_test.go
+++ client/client_test.go
@@ -11,10 +11,3 @@ func TestNew_public(t *testing.T) {
 	c := NewClient("api-key", "org-name", "https://api.lightstep.com")
 	assert.Equal(t, "https://api.lightstep.com/public/v0.2/org-name", c.baseUrl)
 }
-
-func TestNew_env_var_provided_baseURL(t *testing.T) {
-	// Parallel not used here due to t.Setenv.
-	t.Setenv("LIGHTSTEP_API_BASE_URL", "http://localhost:8080")
-	c := NewClient("api-key", "org-name", "")
-	assert.Equal(t, "http://localhost:8080/public/v0.2/org-name", c.baseUrl)
-}
