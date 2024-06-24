2173d00ea13d75867de0bbc5dc29986e3fc917cf
srjames90 <srjames90@berkeley.edu>
2024-06-24 16:28:20 -0400

    remove color validation

--- lightstep/resource_dashboard.go
+++ lightstep/resource_dashboard.go
@@ -14,21 +14,6 @@ func getThresholdSchema() map[string]*schema.Schema {
 		"color": {
 			Type:     schema.TypeString,
 			Required: true,
-			ValidateFunc: validation.StringInSlice([]string{
-				"#AA3018",
-				"#B67D0C",
-				"#3C864F",
-				"#1B7BBB",
-				"#DC7847",
-				"#78469B",
-				"#37A2AE",
-				"#B03B7F",
-				"#1F40C1",
-				"#8B7255",
-				"#826CEF",
-				"#D56DD5",
-				"#6699CC",
-			}, false),
 		},
 		"label": {
 			Type:     schema.TypeString,
